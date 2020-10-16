bf3998c9c25e6fc723232a829b6a3da2351b4e3d
Collet Yves <Yves.Collet@loro.ch>
2020-10-16 09:58:22 +0200

    Ajoutez des fichiers projet.

---
+++ WpfHelpersLibrary/ViewModel/BaseViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ViewModel
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void FirePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
